768d408f7dbd536079c84c9ca682b5b5815fc14e
DMYTRO BLUDOV <weltix@gmail.com>
2019-04-24 16:56:15 +0300

    24.04.2019 temp

--- src/sample/Receipt.java
+++ src/sample/Receipt.java
@@ -4,9 +4,31 @@
 
 package sample;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class Receipt {
-    private List<ReceiptItem> receiptItems;
+/**
+ * @author Dmitriy Bludov
+ */
 
+public class Receipt {
+    private List<ReceiptItem> receiptItems = Collections.synchronizedList(new ArrayList<>());
+
+    void addItem(ReceiptItem item) {
+        if (!receiptItems.contains(item))
+            receiptItems.add(item);
+        else {
+            ReceiptItem existingItem = receiptItems.get(receiptItems.indexOf(item));
+            existingItem.addQuantity(item.getQuantity());
+        }
+    }
+
+    void removeItem(int itemIndex) {
+        receiptItems.remove(itemIndex);
+    }
+
+    void setItemQuantity(int itemIndex, float newQuantity){
+        receiptItems.get(itemIndex).setQuantity(newQuantity);
+    }
 }
--- src/sample/ReceiptItem.java
+++ src/sample/ReceiptItem.java
@@ -4,17 +4,69 @@
 
 package sample;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+
+/**
+ * @author Dmitriy Bludov
+ */
+
 public class ReceiptItem {
+    private String name;
+    // TODO: 24.04.2019 barcode, code and vendoreCode best types?
     private String barcode;
     private int code;
     private int vendorCode;
-    private String name;
 
+    private boolean isDiscountAllowed;
+    private boolean isExcisable;
+    private boolean isDividable;
+    private boolean isFreePrice;
+    private boolean isFiscal;
     private char tax1;
     private char tax2;
-    private boolean isFiscal;
-    private boolean isDividable;
-    private boolean isExcisable;
-    private boolean isDiscountAllowed;
-    private boolean isFreePrice;
+    private BigDecimal price;
+
+    private float quantity = 1.0f;
+
+    void setPrice(BigDecimal newPrice) {
+        this.price = newPrice;
+    }
+
+    void addQuantity(float quantity) {
+        this.quantity += quantity;
+    }
+
+    void setQuantity(float newQuantity) {
+        this.quantity = newQuantity;
+    }
+
+    float getQuantity() {
+        return quantity;
+    }
+
+    // TODO: 24.04.2019 Update equals and hashCode methods
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ReceiptItem that = (ReceiptItem) o;
+        return code == that.code &&
+                vendorCode == that.vendorCode &&
+                isDiscountAllowed == that.isDiscountAllowed &&
+                isExcisable == that.isExcisable &&
+                isDividable == that.isDividable &&
+                isFreePrice == that.isFreePrice &&
+                isFiscal == that.isFiscal &&
+                tax1 == that.tax1 &&
+                tax2 == that.tax2 &&
+                Objects.equals(name, that.name) &&
+                barcode.equals(that.barcode) &&
+                Objects.equals(price, that.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, barcode, code, vendorCode, isDiscountAllowed, isExcisable, isDividable, isFreePrice, isFiscal, tax1, tax2, price);
+    }
 }
